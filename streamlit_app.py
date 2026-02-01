295d939a3ead3cbf753b9917fd512ebd70407349
Arham sheikh <151719975+arhamsheikh17@users.noreply.github.com>
2026-02-02 00:45:12 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
-from snowflake.snowpark.context import get_active_session
+import streamlit as st
+from snowflake.snowpark.functions import col
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 
 
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'),col('SEARCH_ON'))
